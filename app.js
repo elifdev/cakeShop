98033e8877e6f4ed13c44f63f23f6e9fedd08f75
elifdev <elifdevpro@gmail.com>
2025-03-03 22:19:24 +0300

    update

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 const menuBtn = document.getElementById("menu-btn");
 const navMenu = document.getElementById("nav-menu");
 const menuBtnIcon = menuBtn.querySelector("i");
+const letsEatBtn = document.getElementById("letsEatBtn");
+const reservationBtn = document.getElementById("reservationBtn");
 
-menuBtn.addEventListener("click" , () => {
+
+menuBtn.addEventListener("click", () => {
     navMenu.classList.toggle("open");
 
     const isOpen = navMenu.classList.contains("open");
     menuBtnIcon.setAttribute(
-        "class" , 
+        "class", 
         isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars"
     );
 });
 
-navMenu.addEventListener("click" , () => {
+navMenu.addEventListener("click", () => {
     navMenu.classList.remove("open");
-    menuBtnIcon.setAttribute("class" , "fa-solid fa-bars");
+    menuBtnIcon.setAttribute("class", "fa-solid fa-bars");
 });
+
+
+letsEatBtn.addEventListener("click", () => {
+    document.querySelector("#menu").scrollIntoView({ behavior: "smooth" });
+});
+
+reservationBtn.addEventListener("click", () => {
+    document.querySelector(".note").scrollIntoView({ behavior: "smooth" });
+});
+
 
 const scrollRevealOption = {
     distance: "50px",
     origin: "bottom",
     duration: 1000,
-
 };
-
 
 ScrollReveal().reveal(".header-container h1", {
     ...scrollRevealOption,
